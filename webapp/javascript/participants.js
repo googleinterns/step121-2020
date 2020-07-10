9a73c0ddd78abec4eba3d336e0e11c18d999dc3c
Asha Ivey <ashavivey@gmail.com>
2020-07-10 20:52:25 +0000

    Change indentation for HTML and JS files to match Google's style.

--- webapp/javascript/participants.js
+++ webapp/javascript/participants.js
@@ -1,11 +1,11 @@
 /** Some Hard-coded data to display as participants temporarily. */
 var participant1 = {
-    name: "Spider-man",
-    location: "New York"
+  name: "Spider-man",
+  location: "New York"
 };
 var participant2 = {
-    name: "Batman",
-    location: "Gotham City"
+  name: "Batman",
+  location: "Gotham City"
 };
     
 var allParticipants = [participant1, participant2];
@@ -18,23 +18,23 @@ var allParticipants = [participant1, participant2];
  * can be used as a template for when we get data from Datastore.
  */
 function showParticipants() {
-    var participantContainer = document.getElementById('participant-container');
+  var participantContainer = document.getElementById('participant-container');
 
-    //This will be used to create a new div for every participant grabbed from Datastore:
-    allParticipants.forEach(participant => {
-        var newDiv = document.createElement('div');
-        newDiv.classList.add('participant')
+  //This will be used to create a new div for every participant grabbed from Datastore:
+  allParticipants.forEach(participant => {
+    var newDiv = document.createElement('div');
+    newDiv.classList.add('participant');
 
-        var name = document.createElement('p');
-        name.classList.add('participant-name');
-        name.appendChild(document.createTextNode(participant.name));
-        newDiv.appendChild(name);
+    var name = document.createElement('p');
+    name.classList.add('participant-name');
+    name.appendChild(document.createTextNode(participant.name));
+    newDiv.appendChild(name);
 
-        var location = document.createElement('p');
-        location.classList.add('participant-info');
-        location.appendChild(document.createTextNode(participant.location));
-        newDiv.appendChild(location);
+    var location = document.createElement('p');
+    location.classList.add('participant-info');
+    location.appendChild(document.createTextNode(participant.location));
+    newDiv.appendChild(location);
 
-        participantContainer.appendChild(newDiv);
-    });
+    participantContainer.appendChild(newDiv);
+  });
  }
