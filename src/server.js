79f1e83db747ec3902094ebfe093d16c9a12495b
Vedant Roy <vroy101@gmail.com>
2020-07-15 15:41:35 -0400

    Merge front-end and backend

--- src/server.js
+++ src/server.js
@@ -8,9 +8,14 @@ const KIND_EVENT = "Event";
 
 const URL_PARAM_EVENT_ID = `eventID`;
 
+// TODO(ved): There's definitely a cleaner way to do this.
+const PREFIX_API = "/api"
+
 const ERROR_BAD_DB_INTERACTION = "BAD_DATABASE";
 const ERROR_INVALID_EVENT_ID = "INVALID_EVENT_ID";
 const ERROR_BAD_UUID = "BAD_UUID";
+
+app.use(express.static("static"))
 
 // Parse request bodies with the json content header into JSON
 app.use(express.json());
@@ -108,7 +113,7 @@ async function getEvent(request, response, next) {
     });
 }
 
-app.post("/create", async (_, response) => {
+app.post(`${PREFIX_API}/create`, async (_, response) => {
   const key = datastore.key([KIND_EVENT]);
   const result = await datastore.save({ key, data: { users: {} } });
   response.send({
@@ -122,7 +127,9 @@ app.post("/create", async (_, response) => {
   });
 });
 
-app.post(`/:${URL_PARAM_EVENT_ID}`, getEvent, async (request, response) => {
+console.log(`${PREFIX_API}/create`)
+
+app.post(`${PREFIX_API}/:${URL_PARAM_EVENT_ID}`, getEvent, async (request, response) => {
   const { body, datastoreKey: key, event } = request;
   const location = JSON.parse(body.location);
   const [lat, long] = location;
@@ -146,11 +153,11 @@ app.post(`/:${URL_PARAM_EVENT_ID}`, getEvent, async (request, response) => {
     });
 });
 
-app.get(`/:${URL_PARAM_EVENT_ID}/details`, getEvent, async (request, response) => {
+app.get(`${PREFIX_API}/:${URL_PARAM_EVENT_ID}/details`, getEvent, async (request, response) => {
   response.json({ status: 200, data: request.event });
 });
 
-app.get(`/:${URL_PARAM_EVENT_ID}/me`, getEvent, async (request, response) => {
+app.get(`${PREFIX_API}/:${URL_PARAM_EVENT_ID}/me`, getEvent, async (request, response) => {
   const { event } = request;
   const users = event.users || {};
   const userInfo = users[request.session.userID] || {};
@@ -160,7 +167,5 @@ app.get(`/:${URL_PARAM_EVENT_ID}/me`, getEvent, async (request, response) => {
   });
 });
 
-const port = 3000;
-app.listen(port, () =>
-  console.log(`Server listening at http://localhost:${port}`)
-);
+const port = 8080;
+app.listen(port, () => console.log(`Server listening on port: ${port}`))
