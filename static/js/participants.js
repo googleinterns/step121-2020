e5d66cb7d58e1a778747aedd42a16c43f468e7b8
Vedant Roy <vroy101@gmail.com>
2020-07-15 17:31:53 -0400

    formatting

--- static/js/participants.js
+++ static/js/participants.js
@@ -2,39 +2,37 @@
 
 const participant1 = {
   name: "Spider-man",
-  location: "New York"
+  location: "New York",
 };
 const participant2 = {
-
   name: "Batman",
-  location: "Gotham City"
+  location: "Gotham City",
 };
-    
+
 const allParticipants = [participant1, participant2];
 
 /**
  * Creates HTML elements for the participant details and adds it
  * to participants.html.
  *
- * For now, this function deals with hard-coded data, but this 
+ * For now, this function deals with hard-coded data, but this
  * can be used as a template for when we get data from Datastore.
  */
 function showParticipants() {
-  const participantContainer = document.getElementById('participant-container');
+  const participantContainer = document.getElementById("participant-container");
 
   //This will be used to create a new div for every participant grabbed from Datastore:
-  allParticipants.forEach(participant => {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('participant-card');
+  allParticipants.forEach((participant) => {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("participant-card");
 
-    let name = document.createElement('h2');
-    name.classList.add('participant-name');
+    let name = document.createElement("h2");
+    name.classList.add("participant-name");
     name.appendChild(document.createTextNode(participant.name));
     newDiv.appendChild(name);
 
-
-    let location = document.createElement('p');
-    location.classList.add('participant-info');
+    let location = document.createElement("p");
+    location.classList.add("participant-info");
     location.appendChild(document.createTextNode(participant.location));
     newDiv.appendChild(location);
 
--- static/js/searchPage.js
+++ static/js/searchPage.js
@@ -2,62 +2,64 @@
 const restaurant1 = {
   name: "Nice Cafe",
   address: "123 N. Street st.",
-  latLong: {lat: 37.0000, lng: -122.0000},
+  latLong: { lat: 37.0, lng: -122.0 },
   rating: 5,
   priceLevel: 2,
-  openingHours: "8:00am - 10:00pm"
+  openingHours: "8:00am - 10:00pm",
 };
 const restaurant2 = {
   name: "Bad Cafe",
   address: "987 S. Street st.",
-  latLong: {lat: -37.0000, lng: 122.0000},
+  latLong: { lat: -37.0, lng: 122.0 },
   rating: 2,
   priceLevel: 4,
-  openingHours: "12:00am - 9:00am"
+  openingHours: "12:00am - 9:00am",
 };
-    
+
 const allRestaurants = [restaurant1, restaurant2];
 
 /**
  * Creates HTML elements for the restaurant details and adds it
  * to a container in searchResults.html.
  *
- * For now, this function deals with hard-coded data, but this 
+ * For now, this function deals with hard-coded data, but this
  * can be used as a template for when we get data the Places Library results.
  */
 function showRestaurants() {
-  const restaurantContainer = document.getElementById('restaurant-container');
+  const restaurantContainer = document.getElementById("restaurant-container");
 
   //This will be used to create a new div for every restaurant returned by the Places Library:
-  allRestaurants.forEach(restaurant => {
-    let restaurantDiv = document.createElement('div');
-    restaurantDiv.classList.add('restaurant-card');
+  allRestaurants.forEach((restaurant) => {
+    let restaurantDiv = document.createElement("div");
+    restaurantDiv.classList.add("restaurant-card");
 
     // Add information to the left side of the restaurant card. This contains name and atmospheric information
-    let leftDiv = document.createElement('div');
+    let leftDiv = document.createElement("div");
 
-    let name = document.createElement('h2');
-    name.classList.add('restaurant-name');
+    let name = document.createElement("h2");
+    name.classList.add("restaurant-name");
     name.appendChild(document.createTextNode(restaurant.name));
     leftDiv.appendChild(name);
 
-    let location = document.createElement('p');
-    location.classList.add('restaurant-info');
-    location.appendChild(document.createTextNode("Rating: " + restaurant.rating));
+    let location = document.createElement("p");
+    location.classList.add("restaurant-info");
+    location.appendChild(
+      document.createTextNode("Rating: " + restaurant.rating)
+    );
     leftDiv.appendChild(location);
 
     restaurantDiv.appendChild(leftDiv);
 
     //Add information to the left side of the restaurant card. This contains contact information.
-    let rightDiv = document.createElement('div');
+    let rightDiv = document.createElement("div");
 
-    let address = document.createElement('p');
-    address.classList.add('restaurant-basic-info');
+    let address = document.createElement("p");
+    address.classList.add("restaurant-basic-info");
     address.appendChild(document.createTextNode(restaurant.address));
     rightDiv.appendChild(address);
 
-    let openingHours = document.createElement('p');
-    openingHours.classList.add('restaurant-basic-info');
+    let openingHours = document.createElement("p");
+    openingHours.classList.add("restaurant-basic-info");
     openingHours.appendChild(document.createTextNode(restaurant.openingHours));
     rightDiv.appendChild(openingHours);
 
