5f422d314e11969ff8f42b9e62fedbc433eb3bdf
Vedant Roy <vroy101@gmail.com>
2020-07-15 20:58:12 +0000

    Use datastore in production

--- src/datastore.js
+++ src/datastore.js
@@ -2,14 +2,23 @@ require("dotenv").config();
 const { env } = require("process");
 const { Datastore } = require("@google-cloud/datastore");
 
-if (!env.DATASTORE_HOST || !env.DATASTORE_PORT || !env.DATASTORE_PROJECT_ID) {
-  console.error(`missing one of required config values`);
-  process.exit(1);
+let datastore;
+if (env.NODE_ENV === "production") {
+  // The NODE_ENV environment variable is set
+  // automatically by Google App Engine in production
+  datastore = new Datastore({
+    projectId: env.DATASTORE_PROJECT_ID,
+  });
+} else {
+  // we're in local
+  if (!env.DATASTORE_HOST || !env.DATASTORE_PORT || !env.DATASTORE_PROJECT_ID) {
+    console.error(`missing one of required config values`);
+    process.exit(1);
+  }
+  datastore = new Datastore({
+    projectId: env.DATASTORE_PROJECT_ID,
+    apiEndpoint: `${env.DATASTORE_HOST}:${env.DATASTORE_PORT}`,
+  });
 }
-
-const datastore = new Datastore({
-  projectId: env.DATASTORE_PROJECT_ID,
-  apiEndpoint: `${env.DATASTORE_HOST}:${env.DATASTORE_PORT}`,
-});
 
 module.exports = datastore;
